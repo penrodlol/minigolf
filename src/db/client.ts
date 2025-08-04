bac31dd26ba5bd771e52d7b7144f09a30cdde412
penrodlol <christian@penrod.contact>
2025-08-04 17:00:26 -0400

    🧱 updates

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,5 +3,6 @@ import { openDatabaseSync } from 'expo-sqlite';
 import * as schema from './schema';
 
 export const sqlite = openDatabaseSync('minigolf.db', { enableChangeListener: true });
+sqlite.execSync('PRAGMA journal_mode = WAL');
 sqlite.execSync('PRAGMA foreign_keys = ON');
 export const db = drizzle(sqlite, { schema });
